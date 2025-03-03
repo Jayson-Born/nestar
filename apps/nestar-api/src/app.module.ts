8b8cdd1ebdfc2129557c9867d94ff0e2a313ab72
Jayson-Born <abdukabir34@gmail.com>
2025-03-04 00:09:48 +0900

    feat:integration GraphQLModule and build firstGraphQL api

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule} from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { NestarBatchController } from 'apps/nestar-batch/src/nestar-batch.controller';
+import { NestarBatchService } from 'apps/nestar-batch/src/nestar-batch.service';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+ imports: [ConfigModule.forRoot(),
+     GraphQLModule.forRoot({
+       driver: ApolloDriver,
+       playground: true,
+       uploads: false,
+       autoSchemaFile: true,
+     }),
+   ],
+   controllers: [NestarBatchController],
+   providers: [NestarBatchService, AppResolver],
 })
 export class AppModule {}
