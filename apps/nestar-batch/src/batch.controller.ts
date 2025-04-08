fe8a5cd2dc8df90d677beff07f8279c06fdfba54
Jayson-Born <abdukabir34@gmail.com>
2025-04-08 18:13:39 +0900

    feat: create batchProperty and batchAgents jobSchedule logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,56 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class BatchController {
-  constructor(private readonly nestarBatchService: BatchService) {}
+  private logger: Logger = new Logger("BatchController");
+  constructor(private readonly BatchService: BatchService) {}
+
+  @Timeout(1000)
+  handleTimeout() {
+    this.logger.debug('BATCH SERVER READY!');
+  }
+
+  @Cron('00 * * * * *', {name: BATCH_ROLLBACK})
+  public batchRollback() {
+    try{this.logger['context'] = BATCH_ROLLBACK;
+    this.logger.debug('EXECUTED!');
+  } catch(err) {
+    this.logger.error(err);
+  }
+
+}
+
+  @Cron('20 * * * * *', {name: BATCH_TOP_PROPERTIES})
+  public batchProperties() {
+    try{ this.logger['context'] = BATCH_TOP_PROPERTIES;
+    this.logger.debug('EXECUTED!');
+  } catch(err) {
+    this.logger.error(err);
+  }
+}
+
+  @Cron('40 * * * * *', {name: BATCH_TOP_AGENTS})
+  public batchAgents() {
+     try{this.logger['context'] = BATCH_TOP_AGENTS;
+    this.logger.debug('EXECUTED!');
+  }catch(err) {
+    this.logger.error(err);
+  }
+  }
+
+
+
+  // @Interval(1000)
+  // handleTimeout() {
+  //   this.logger.debug('BATCH SERVER READY!');
+  // }
+
 
   @Get()
   getHello(): string {
-    return this.nestarBatchService.getHello();
+    return this.BatchService.getHello();
   }
 }
--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -5,4 +5,14 @@ export class BatchService {
   getHello(): string {
     return 'Welcom to Nestar BATCH Server!';
   }
+
+  public async batchRollback(): Promise<void> {
+    console.log('BATCH ROLLBACK');
+}
+  public async batchProperties(): Promise<void> {
+    console.log('BATCH TOP PROPERTIES');
+}
+  public async batchAgents(): Promise<void> {
+    console.log('BATCH TOP AGENTS');
+}
 }
